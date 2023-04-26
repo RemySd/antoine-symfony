78ba07809e48e72ef423672202f0f8d9cc130690
Mantoine180 <claverie.tonio@gmail.com>
2023-04-26 22:06:31 +0200

    Modification de l'édition des intervenants

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -2,30 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use App\Entity\Intervenant;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class IntervenantFormType extends AbstractType
+class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'firstname',
-            ])
-
+            ->add('lastname')
+            ->add('firstname')
+            ->add('email')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Intervenant::class,
+            'data_class' => User::class,
         ]);
     }
 }
